1bb19120db9bc947889c2bc6fa4e184fc7326314
Daniel Andreas Wang <daniel.wang@try.no>
2025-04-04 21:28:25 +0200

    next project with storybook

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    // "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/experimental-nextjs-vite",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ]
+};
+
+export default config;
